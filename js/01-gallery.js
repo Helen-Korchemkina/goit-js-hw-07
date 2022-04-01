ec2bd3df11145165d6a0e032a80fb06c66bda8a1
Helen <dd040388kev@gmail.com>
2022-04-01 22:21:27 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryItemsEls = document.querySelector('.gallery');
+const galleryItemsEls = document.querySelector(".gallery");
 
 const makeGalleryItems = createGalleryItems(galleryItems);
-galleryItemsEls.insertAdjacentHTML('beforeend', makeGalleryItems);
+galleryItemsEls.insertAdjacentHTML("beforeend", makeGalleryItems);
 
 function createGalleryItems(gallery) {
-    return gallery.map(({ preview, original, description }) => {
-        return `
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
 <div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -20,17 +21,22 @@ function createGalleryItems(gallery) {
   </a>
 </div>
         `;
-    }).join('');
-    }
-
-galleryItemsEls.addEventListener('click', onOpenModal);
-
-function onOpenModal(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    
+    })
+    .join("");
 }
 
+galleryItemsEls.addEventListener("click", onOpenModal);
 
+function onOpenModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instance.element().querySelector(".modal").src = event.target.dataset.source;
+  instance.show();
+}
+
+const instance = basicLightbox.create(`
+    <img src="" class="modal">
+`);
