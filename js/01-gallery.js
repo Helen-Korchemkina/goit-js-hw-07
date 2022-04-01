1861593e77e84a29b328a056c05d2401a4e46938
Helen <dd040388kev@gmail.com>
2022-04-01 20:46:10 +0300

    non

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsEls = document.querySelector('.gallery');
+
+const makeGalleryItems = createGalleryItems(galleryItems);
+galleryItemsEls.insertAdjacentHTML('beforeend', makeGalleryItems);
+
+function createGalleryItems(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    }).join('');
+    }
+
+galleryItemsEls.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+}
+
+
